a29ca3fa6d00a54be851caa38a16ad849339a70c
Kamilovvqismet@gmail.com <Kamilovvqismet@gmail.com>
2024-03-06 00:56:22 +0400

    i dont fix it

--- src/test/java/az/keytd/expensetracker/service/ExpenseTrackerTestService.java
+++ src/test/java/az/keytd/expensetracker/service/ExpenseTrackerTestService.java
@@ -5,18 +5,23 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import az.keytd.expensetracker.dto.LoginRequest;
 import az.keytd.expensetracker.dto.RegisterRequest;
 import az.keytd.expensetracker.dto.Response;
+import az.keytd.expensetracker.entities.CommonOtp;
+import az.keytd.expensetracker.entities.OtpStatus;
 import az.keytd.expensetracker.entities.User;
+import az.keytd.expensetracker.repository.CommonOtpRepository;
 import az.keytd.expensetracker.repository.UserRepository;
 import az.keytd.expensetracker.security.JwtService;
 
@@ -30,6 +35,12 @@ public class ExpenseTrackerTestService {
     @MockBean
     private UserService userService;
 
+    @MockBean
+    private SimpleMailMessage simpleMailMessage;
+
+    @MockBean
+    private CommonOtpRepository commonOtpRepository;
+    
     @MockBean
     private CommonOtpService otpService;
 
@@ -93,5 +104,35 @@ public class ExpenseTrackerTestService {
         assertEquals("ok", response.getMessage());
         assertEquals(token, response.getData());
 
+    }
+
+    @Test
+    public void sendByEmail_shouldSendOtp(){
+        String to = "qismet600@gmail.com";
+
+        when(otpService.generateOtp()).thenReturn("123456");
+
+        otpService.sendByEmail(to);
+
+        verify(otpService, times(1)).sendByEmail(to);
+    }
+
+    @Test
+    public void verify_validOtp(){
+        String email = "qismet600@gmail.com";
+        String otp = "123456";
+
+        CommonOtp commonOtp = new CommonOtp();
+        commonOtp.setEmail(email);
+        commonOtp.setOtp(otp);
+        commonOtp.setStatus(OtpStatus.CONFIRMED);
+
+        when(commonOtpRepository.findByEmail(email)).thenReturn(Optional.of(commonOtp));
+
+        authenticationService.verify(email, otp);
+
+        assertEquals(OtpStatus.CONFIRMED, commonOtp.getStatus());
+        
+        verify(commonOtpRepository, times(1)).save(commonOtp);
     }
 }
